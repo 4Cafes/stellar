cbe221d56bd021e4d77223b623b3af73a993a8cd
4Cafes <138708258+4Cafes@users.noreply.github.com>
2023-10-14 16:58:43 -0700

    Add files via upload

--- files/parrot.py
+++ files/parrot.py
@@ -1,1 +1,14 @@
+import subprocess
+import time
 
+print("Cafes.lol")
+time.sleep(3)
+
+command = 'curl parrot.live'
+
+try:
+    subprocess.run(command, shell=True, check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
